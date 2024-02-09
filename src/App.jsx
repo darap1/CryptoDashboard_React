9e95470d15f873b42ba239a82d2af00e15d621dc
darap1 <darap@mail.ru>
2024-02-09 23:06:08 +0100

    feat: create AppSider component

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,31 @@
+import { Layout } from 'antd';
+import AppHeader from './components/Layout/AppHeader';
+import AppSider from './components/Layout/AppSider';
+
+const headerStyle = {
+	textAlign: 'center',
+	color: '#fff',
+	height: 60,
+	paddingInline: 48,
+	lineHeight: '64px',
+	backgroundColor: '#4096ff',
+};
+const contentStyle = {
+	textAlign: 'center',
+	minHeight: 'calc(100vh - 60px)',
+	color: '#fff',
+	backgroundColor: '#001529',
+};
+
 export default function App() {
-  return <h1>React Crypto App</h1>
+	return (
+		<Layout>
+			<AppHeader />
+			<Layout>
+				<AppSider />
+				<Layout.Content style={contentStyle}>Content</Layout.Content>
+			</Layout>
+		</Layout>
+	);
 }
+
