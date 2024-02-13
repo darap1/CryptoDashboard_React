0b42bf1371c782ad2616e4b061c8c99688f54b7b
darap1 <darap@mail.ru>
2024-02-13 23:32:13 +0100

    feat: add capitalizeFunc

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,6 @@
-export default function percentDiff(a, b) {
+export function percentDiff(a, b) {
 	return +(100 * Math.abs((a - b) / ((a + b) / 2)).toFixed(2));
+}
+export function capitalize(str) {
+	return str.charAt(0).toUpperCase() + str.substr(1);
 }
