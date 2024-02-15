28f8af7b755c85d25ebfbfb26cfa28b81a96b10e
darap1 <darap@mail.ru>
2024-02-15 18:56:36 +0100

    feat: add AppLayout component

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,11 @@
-import { Layout } from 'antd';
-import AppHeader from './components/Layout/AppHeader';
-import AppSider from './components/Layout/AppSider';
-import AppContent from './components/Layout/AppContent';
+import AppLayout from './components/Layout/AppLayout';
+import { CryptoContextProvider } from './context/crypto-context';
 
 export default function App() {
 	return (
-		<Layout>
-			<AppHeader />
-			<Layout>
-				<AppSider />
-				<AppContent />
-			</Layout>
-		</Layout>
+		<CryptoContextProvider>
+			<AppLayout />
+		</CryptoContextProvider>
 	);
 }
 
